93afb60b5f8db97a074e4df7cb281df638b06696
Hamza Ahmed <hamza.ah@berkeley.edu>
2016-09-01 14:08:41 -0700

    Update index.html

--- components/follow.jsx
+++ components/follow.jsx
@@ -1,12 +1,14 @@
 var ContactItem = React.createClass({
   render: function () {
     return (
+      <a target="_blank" href={this.props.link}>
         <div className="followItem">
           <i className={this.props.icon + " followIcon"}></i>
           <div className="followIconName">
             {this.props.name}
           </div>
         </div>
+      </a>
     );
   }
 });
@@ -19,6 +21,7 @@ var ContactList = React.createClass({
           key={index}
           name={contactItem.name}
           icon={contactItem.icon}
+          link={contactItem.link}
         />
       );
     });
@@ -32,8 +35,8 @@ var ContactList = React.createClass({
 });
 
 var contactItems = [
-  { name: "barbellatberkeley", icon: "fa fa-instagram" },
-  { name: "Barbell at Berkeley", icon: "fa fa-facebook" }
+  { name: "barbellatberkeley", icon: "fa fa-instagram", link: "https://instagram.com/berkeleybarbellclub" },
+  { name: "Barbell at Berkeley", icon: "fa fa-facebook", link: "https://www.facebook.com/groups/537417056451110/" }
 ];
 
 ReactDOM.render(<ContactList contactItems={contactItems} />, document.getElementById('follow'));
